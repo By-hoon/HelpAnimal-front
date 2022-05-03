cde91d9bdbde7ca73bab4204e03c2d62d6221897
Yeunhun Bae <sbaru1997@naver.com>
2022-05-03 17:19:42 +0900

    HA-35 Frontend : 크루 메인 페이지 구현

--- src/routes/Crew.tsx
+++ src/routes/Crew.tsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+import CrewCard from "../components/CrewCard";
+import { crews } from "../test/data";
+
 const Crew = () => {
-  return <span>crew</span>;
+  return (
+    <div className="crew__container">
+      <div className="title__container">
+        <span className="title__span">크루 목록</span>
+        <Link className="create-crew__link" to="/crew/add">
+          크루 생성
+        </Link>
+      </div>
+      <div className="crew-list__container">
+        {crews.map((crew) => (
+          <CrewCard
+            key={crew.id}
+            id={crew.id}
+            logo={crew.logo}
+            name={crew.name}
+            introduction={crew.introduction}
+            amount={crew.amount}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Crew;
