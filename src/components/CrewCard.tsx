0d151d26182375117cf8014a1fc3625907e62561
By-hoon <sbaru1997@naver.com>
2022-06-22 19:14:31 +0900

    HA-40 Frontend : 크루 무한 스크롤 구현
    - Intersection Observer 사용

--- src/components/CrewCard.tsx
+++ src/components/CrewCard.tsx
@@ -1,10 +1,16 @@
+import { Dispatch, SetStateAction } from "react";
 import { Link } from "react-router-dom";
 import { CrewProps } from "../shared/Props";
 
-const CrewCard = (crew: CrewProps) => {
+interface CrewCardProps {
+  crew: CrewProps;
+  setLastCrew: Dispatch<SetStateAction<HTMLAnchorElement | null | undefined>>;
+}
+
+const CrewCard = ({ crew, setLastCrew }: CrewCardProps) => {
   return (
     <div className="card__container">
-      <Link to={`/crew/detail`} state={{ crewId: crew.id }} className="link--perspective">
+      <Link to={`/crew/detail`} state={{ crewId: crew.id }} ref={setLastCrew} className="link--perspective">
         <div className="card__container--forward">
           <div className="logo__container">
             <img src={crew.logo} alt="crew-logo" className="logo__image" />
--- src/components/Crews.tsx
+++ src/components/Crews.tsx
@@ -1,21 +1,44 @@
-import { crews } from "../test/data";
+import { useEffect, useState } from "react";
+import { CrewProps } from "../shared/Props";
+import { CrewsData } from "../test/data";
 import CrewCard from "./CrewCard";
 import { CrewListTitle } from "./Title";
 
 const Crews = () => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const [crews, setCrews] = useState([]);
+  const [lastCrew, setLastCrew] = useState<HTMLAnchorElement | null>();
+
+  const onIntersect: IntersectionObserverCallback = (playlists, observer) => {
+    playlists.forEach((playlist) => {
+      if (playlist.isIntersecting) {
+        if (CrewsData[pageIndex + 1]) {
+          setPageIndex(pageIndex + 1);
+        }
+        observer.unobserve(playlist.target);
+      }
+    });
+  };
+
+  useEffect(() => {
+    setCrews(crews.concat(CrewsData[pageIndex]));
+  }, [pageIndex]);
+
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    if (lastCrew) {
+      observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
+      observer.observe(lastCrew);
+    }
+    return () => observer && observer.disconnect();
+  }, [lastCrew]);
+
   return (
     <div className="crew__container">
       <CrewListTitle />
       <div className="crew-list__container">
-        {crews.map((crew) => (
-          <CrewCard
-            key={crew.id}
-            id={crew.id}
-            logo={crew.logo}
-            name={crew.name}
-            introduction={crew.introduction}
-            amount={crew.amount}
-          />
+        {crews.map((crew: CrewProps) => (
+          <CrewCard key={crew.id} crew={crew} setLastCrew={setLastCrew} />
         ))}
       </div>
     </div>
