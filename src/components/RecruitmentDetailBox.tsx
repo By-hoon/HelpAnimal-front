ddb0ee008d0a3efbf77a6fbf71c49be5615cd1ee
Yeunhun Bae <sbaru1997@naver.com>
2022-08-03 20:14:06 +0900

    HA-49 Frontend : 플로팅 메뉴 컴포넌트 스타일링

--- src/components/RecruitmentDetailBox.tsx
+++ src/components/RecruitmentDetailBox.tsx
@@ -38,43 +38,47 @@ const RecruitmentDetailBox = () => {
 
   return (
     <>
-      <div className="recruitment-detail__container">
-        <div className="recruitment-detail-images__container">
-          <img src={images[currentImage]} alt="recruitment-images" className="recruitment-detail__image" />
-          <div className="recruitment-detail-image-buttons__container">
-            <div className="recruitment-detail-image-button__container pointer" onClick={showLeftImage}>
-              <Icon icon="ant-design:left-outlined" />
-            </div>
-            <div className="recruitment-detail-image-button__container pointer" onClick={showRightImage}>
-              <Icon icon="ant-design:right-outlined" />
-            </div>
-          </div>
-        </div>
-        <div className="recruitment-detail-header__container">
-          <div className="recruitment-detail-title__container">
-            <p className="recruitment-detail-name__p">{recruitmentDetailData.name}</p>
-            <div className="recruitment-detail-subs__container">
-              <span className="recruitment-detail-author__span" onClick={goAuthorPage}>
-                {recruitmentDetailData.author}
-              </span>
-              <span className="recruitment-detail-type__span">[{recruitmentDetailData.recruitmentType}]</span>
-              <span className="recruitment-detail-participant__span">
-                | {recruitmentDetailData.participant}
-              </span>
-              <span className="recruitment-detail-method__span">
-                {recruitmentDetailData.recruitmentMethod}
-              </span>
+      <div className="recruitment-detail-box__container">
+        <div className="recruitment-detail__container">
+          <div className="recruitment-detail-images__container">
+            <img src={images[currentImage]} alt="recruitment-images" className="recruitment-detail__image" />
+            <div className="recruitment-detail-image-buttons__container">
+              <div className="recruitment-detail-image-button__container pointer" onClick={showLeftImage}>
+                <Icon icon="ant-design:left-outlined" />
+              </div>
+              <div className="recruitment-detail-image-button__container pointer" onClick={showRightImage}>
+                <Icon icon="ant-design:right-outlined" />
+              </div>
             </div>
           </div>
-          <div className="recruitment-detail-animal__container">
-            <span className="recruitment-detail-animal__span">{recruitmentDetailData.animalType}</span>
+          <div className="recruitment-detail-header__container">
+            <div className="recruitment-detail-title__container">
+              <p className="recruitment-detail-name__p">{recruitmentDetailData.name}</p>
+              <div className="recruitment-detail-subs__container">
+                <span className="recruitment-detail-author__span" onClick={goAuthorPage}>
+                  {recruitmentDetailData.author}
+                </span>
+                <span className="recruitment-detail-type__span">
+                  [{recruitmentDetailData.recruitmentType}]
+                </span>
+                <span className="recruitment-detail-participant__span">
+                  | {recruitmentDetailData.participant}
+                </span>
+                <span className="recruitment-detail-method__span">
+                  {recruitmentDetailData.recruitmentMethod}
+                </span>
+              </div>
+            </div>
+            <div className="recruitment-detail-animal__container">
+              <span className="recruitment-detail-animal__span">{recruitmentDetailData.animalType}</span>
+            </div>
+          </div>
+          <div className="recruitment-detail-content__container">
+            <div dangerouslySetInnerHTML={{ __html: recruitmentDetailData.content }} />
           </div>
         </div>
-        <div className="recruitment-detail-content__container">
-          <div dangerouslySetInnerHTML={{ __html: recruitmentDetailData.content }} />
-        </div>
+        <FloatingMenu />
       </div>
-      <FloatingMenu />
     </>
   );
 };
