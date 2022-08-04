11de4260acf066a33e0a6b963ae345acbff904ac
Yeunhun Bae <sbaru1997@naver.com>
2022-08-04 20:12:46 +0900

    HA-49 Frontend : 공고 상세 컴포넌트 스타일링

--- src/components/RecruitmentDetailBox.tsx
+++ src/components/RecruitmentDetailBox.tsx
@@ -42,6 +42,11 @@ const RecruitmentDetailBox = () => {
         <div className="recruitment-detail__container">
           <div className="recruitment-detail-images__container">
             <img src={images[currentImage]} alt="recruitment-images" className="recruitment-detail__image" />
+            <img
+              src={images[currentImage]}
+              alt="recruitment-images"
+              className="recruitment-detail__image--background"
+            />
             <div className="recruitment-detail-image-buttons__container">
               <div className="recruitment-detail-image-button__container pointer" onClick={showLeftImage}>
                 <Icon icon="ant-design:left-outlined" />
@@ -54,20 +59,17 @@ const RecruitmentDetailBox = () => {
           <div className="recruitment-detail-header__container">
             <div className="recruitment-detail-title__container">
               <p className="recruitment-detail-name__p">{recruitmentDetailData.name}</p>
-              <div className="recruitment-detail-subs__container">
-                <span className="recruitment-detail-author__span" onClick={goAuthorPage}>
-                  {recruitmentDetailData.author}
-                </span>
-                <span className="recruitment-detail-type__span">
-                  [{recruitmentDetailData.recruitmentType}]
-                </span>
-                <span className="recruitment-detail-participant__span">
-                  | {recruitmentDetailData.participant}
-                </span>
-                <span className="recruitment-detail-method__span">
-                  {recruitmentDetailData.recruitmentMethod}
-                </span>
-              </div>
+            </div>
+            <div className="recruitment-detail-subs__container">
+              <span className="recruitment-detail-author__span" onClick={goAuthorPage}>
+                {recruitmentDetailData.author}
+              </span>
+              <span className="recruitment-detail-type__span">{recruitmentDetailData.recruitmentType}</span>
+              <span className="recruitment-detail-option__span">|</span>
+              <span className="recruitment-detail-option__span">{recruitmentDetailData.participant}</span>
+              <span className="recruitment-detail-option__span">
+                {recruitmentDetailData.recruitmentMethod}
+              </span>
             </div>
             <div className="recruitment-detail-animal__container">
               <span className="recruitment-detail-animal__span">{recruitmentDetailData.animalType}</span>
